d6040cdee281f87353d2dd7359bb25bc6b599cbe
Brad Bensch <bbensch@live.com>
2015-02-21 13:56:06 -0500

    Edit app.js to remove users route

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/crown', crown	);
 app.use('/creation', creation);
 app.use('/science', science);
